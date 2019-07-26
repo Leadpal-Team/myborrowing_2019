b92d88081b6c516b7d65901a18c23dfd2cd145dd
Ellen Long <ellen.renee.01@gmail.com>
2019-07-26 15:57:21 +1000

    Updated text
    Updated text on FAQs and Loan Cost pages

--- inc/components/section-cta-grey.php
+++ inc/components/section-cta-grey.php
@@ -1,13 +1,13 @@
 <section class="pb-0">
-  <div class="outer-container background--cta-grey">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-6 mb-5 color--white">
-          <h2 class="bold">Have a burning question?</h2>
-          <p>Put your feelings into it, your heart, it's your world. You can work and carry-on and put lots of little happy things in here. </p>
-          <a class="color--white btn-link__color-black mt-4" href="apply.myborrowing.com.au/mb/apply">Apply Now</a>
+    <div class="outer-container background--cta-grey">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 mb-5 color--white">
+                    <h2 class="bold">Have a burning question?</h2>
+                    <p>Just get in touch! The friendly team at MyBorrowing are here to answer any questions you may have and help in any way we can. </p>
+                    <a class="color--white btn-link__color-black mt-4" href="apply.myborrowing.com.au/mb/apply">Contact Us</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
--- inc/components/section-cta-yellow.php
+++ inc/components/section-cta-yellow.php
@@ -1,13 +1,13 @@
 <section class="pb-0 ">
-  <div class="outer-container background--cta-yellow">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-6 mb-5">
-          <h2 class="bold">What are you waiting for?</h2>
-          <p>Put your feelings into it, your heart, it's your world. You can work and carry-on and put lots of little happy things in here. </p>
-          <a class="color--white btn-link__color-black-alt mt-4" href="apply.myborrowing.com.au/mb/apply">Apply Now</a>
+    <div class="outer-container background--cta-yellow">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 mb-5">
+                    <h2 class="bold">What are you waiting for?</h2>
+                    <p>Get the finance you need, when you need it. Apply from any device with MyBorrowing and we could find you a lender in as little as 60 seconds! </p>
+                    <a class="color--white btn-link__color-black-alt mt-4" href="apply.myborrowing.com.au/mb/apply">Apply Now</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
