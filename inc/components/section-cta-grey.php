b116206f4812148bdd8475292124615fd95e4e12
SuperSheaff <ben.r.fowler@hotmail.com>
2019-07-29 08:31:18 +1000

    cta linking

--- inc/components/section-cta-grey.php
+++ inc/components/section-cta-grey.php
@@ -5,7 +5,7 @@
                 <div class="col-12 col-md-6 mb-5 color--white">
                     <h2 class="bold">Have a burning question?</h2>
                     <p>Just get in touch! The friendly team at MyBorrowing are here to answer any questions you may have and help in any way we can. </p>
-                    <a class="color--white btn-link__color-black mt-4" href="apply.myborrowing.com.au/mb/apply">Contact Us</a>
+                    <a class="color--white btn-link__color-black mt-4" href="<?php echo get_site_url(); ?>/contact-us/">Contact Us</a>
                 </div>
             </div>
         </div>
