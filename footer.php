b084c911bf909d220db2d007b97d40c89185d344
SuperSheaff <ben.r.fowler@hotmail.com>
2019-08-16 11:58:26 +1000

    disclousre

--- footer.php
+++ footer.php
@@ -54,7 +54,7 @@
 					</div>
 
 					<p class="ts-6 color--dark-grey">
-						Disclaimer: Mifin Pty Ltd 2019 (ABN 14 132 429 595) is a Licensed Financial Broker which is an accredited representative of Marketplace Finance (ACL No. 389029). We are a referral service only and do not directly fund any credit product. Your request will be referred to a third party lender who will contact you directly to ascertain if they can facilitate your requirements. The third party will appraise you of their fees,charges and terms before you would enter any contract with them. We may receive a referral fee (amount not ascertainable) from the third party depending on the outcome of their dealings with you. It is recommended that you obtain independent legal and financial advice before taking up any products that you may be referred to and offered.
+						Disclaimer: MyBorrowing is a trading name of MiFin Pty Ltd (ABN 14 132 429 595), Australian Credit Licence 389029. We are a referral service only and do not directly fund any credit product. Your request will be referred to a third party lender who will contact you directly to ascertain if they can facilitate your requirements. The third party will appraise you of their fees,charges and terms before you would enter any contract with them. We may receive a referral fee (amount not ascertainable) from the third party depending on the outcome of their dealings with you. It is recommended that you obtain independent legal and financial advice before taking up any products that you may be referred to and offered.
 						</p>
 					
 				</div>
